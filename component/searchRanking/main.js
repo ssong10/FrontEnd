28a5ffb8ee3f74b2ce6a0ecbda4516e8b637f841
ssong10 <leesy1403@naver.com>
2021-01-13 23:26:04 +0900

    searchRanking

--- component/searchRanking/main.js
+++ component/searchRanking/main.js
@@ -1,18 +1,20 @@
 const main = document.querySelector('.main'),
   items = document.querySelector('.items'),
   rank = document.querySelector('.rank')
-let currentRank = 0
+
+const itemHeight = 40
+
 const RANKING = [
-  '이승열', '아모아모','대전','싸피','SSAFY',
+  '이승열', '아모아모', '대전', '싸피', 'SSAFY',
   '대천', '관저동', '곱창', '취업', '상무초밥'
 ]
 
-const nextRank = (idx) => {
-  return (idx+1) % 10
-}
-const SHOW = (idx) => {
-  const next = nextRank(idx)
-  return `
+const nextRank = (idx) => (idx + 1) % 10
+
+const slideSetting = (idx) => {
+  items.style.transform = ''
+  next = nextRank(idx)
+  items.innerHTML = `
     <div class="item">
       <div class="search__rank">${idx+1}</div>  
       <div class="search__item">${RANKING[idx]}</div>
@@ -22,24 +24,18 @@ const SHOW = (idx) => {
       <div class="search__item">${RANKING[next]}</div>
     </div>
   `
-} 
-
-const slideSetting = (idx) => {
-  items.style.transform = ''
-  currentRank = nextRank(idx)
-  items.innerHTML = SHOW(idx)
-  for (let i=0;i<38;i++){
-    setTimeout(()=> {
+  for (let i = 0; i < 41; i++) {
+    setTimeout(() => {
       items.style.transform = `translate3d(0px,-${i}px,0px)`
-    },30*i)
+    }, 1800 + 30 * i)
   }
 }
 
-const slideRank = () => {
-  slideSetting(0)
-  setInterval(()=> {
-    slideSetting(currentRank)
-  },3000)
+const slideRank = (idx) => {
+  slideSetting(idx)
+  setTimeout(() => {
+    slideRank(nextRank(idx))
+  }, 3000)
 }
 
-slideRank()
+slideRank(0)
