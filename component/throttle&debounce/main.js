c6b512191b3f5d86ac20a6a38744c6c14bb8e655
ssong10 <leesy1403@naver.com>
2021-02-18 19:30:45 +0900

    throttle & debounce

--- component/throttle&debounce/main.js
+++ component/throttle&debounce/main.js
@@ -2,42 +2,48 @@ const searchInput = document.querySelector('#search')
 const delayType = document.querySelector('select#type')
 const inputLog = document.querySelector('#log')
 const delayInput = document.querySelector('#delay')
-const debounce = (func,delay) => {
+const throttling = (func,delay) => {
   let timer;
   return (...args) => {
     if (!timer) {
       timer = setTimeout(function() {
         timer = null;
-        func(...args,'debounce')
+        func(...args)
       }, delay);
     }
   }
 }
 
-const throttling = (func,delay) => {
+const debounce = (func,delay) => {
   let timer;
   return (...args) => {
     clearTimeout(timer)
     timer = setTimeout(function() {
-      func(...args,'trottling')
+      func(...args)
     }, delay)
   }
 }
 
-const search = (e,type) => {
-  const log = `type : ${type}\ninput : ${e.target.value}`
-  console.log(log)
+const search = () => {
+  const log = `input : ${searchInput.value}`
   inputLog.innerHTML += `<p>${log}</p>`
+  inputLog.scrollTop = inputLog.scrollHeight
 }
 
-const throttledEvent = throttling(search,delayInput.value)
 
-const debouncedEvent = debounce(search,delayInput.value)
-
+let throttledEvent = throttling(search,200)
+let debouncedEvent = debounce(search,200)
+delayInput.addEventListener('change',(e)=> {
+  throttledEvent = throttling(search,Number(e.target.value))
+  debouncedEvent = debounce(search,e.target.value)
+  searchInput.value = ''
+  inputLog.innerHTML += `------- input 초기화 ----------`
+})
 delayType.addEventListener('change', ()=> {
   searchInput.value = ''
   inputLog.innerHTML += `------- input 초기화 ----------`
 })
+
 searchInput.addEventListener('keydown', (e)=> {
   const type = delayType.value
   if (type === 'throttle') {
